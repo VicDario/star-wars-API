c98e76e8cc4ae8062431289425c97396cd95840e
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-01 11:21:55 -0400

    People and Planet model with its serialize method

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
@@ -8,6 +9,9 @@ class People(db.Model):
     height = db.Column(db.String(4), nullable=False)
     mass = db.Column(db.String(4), nullable=False)
     skin_color = db.Column(db.String(10))
+    eye_color = db.Column(db.String(10))
+    birth_year = db.Column(db.String(10))
+    gender = db.Column(db.String(6))
 
     def serialize(self):
         return {
@@ -16,4 +20,31 @@ class People(db.Model):
             "height": self.height,
             "mass": self.mass,
             "skin_color": self.skin_color
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(id.String(25), unique=True)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(15), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(20), nullable=False)
+    terrain = db.Column(db.String(30), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water
         }
