fe1b95792628a40464b2307e8e3130caca7d8027
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-01 14:37:03 -0400

    User and favorite tables added. Endpoints added

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, json, jsonify, request, render_template
 from flask_migrate import Migrate
-from models import Planet, db, People
+from models import Favorite, Planet, User, db, People
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,26 +10,79 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 Migrate(app, db) # init, migrate, upgrade
 
-@app.route('/people', methods=['GET'])
+@app.route('/api/people', methods=['GET'])
 def all_people():
     people = People.query.all()
     people = list(map(lambda people: people.serialize(), people))
     return jsonify(people), 200
 
-@app.route('/people/<int:people_id>', methods=['GET'])
+@app.route('api/people/<int:people_id>', methods=['GET'])
 def get_person(people_id):
     person = People.query.get(people_id)
     person = list(person.serialize())
     return jsonify(person), 200
 
-@app.route('/planet', methods=['GET'])
+@app.route('/api/planet', methods=['GET'])
 def all_planets():
     planets = Planet.query.all()
     planets = list(map(lambda planet: planet.serialize(), planets))
     return jsonify(planets), 200
 
-@app.route('/planet/<int:planet_id>', methods=['GET'])
+@app.route('/api/planet/<int:planet_id>', methods=['GET'])
 def get_planet(planet_id):
     planet = Planet.query.get(planet_id)
     planet = list(planet.serialize())
     return jsonify(planet), 200
+
+@app.route('/api/users', methods=['GET'])
+def all_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
+
+@app.route('/api/users/favorite', methods=['GET'])
+def gets_favorite():
+    favorites = Favorite.query.all()
+    favorites = list(map(lambda favorite: favorite.serialize(), favorites))
+    return jsonify(favorites), 200
+
+@app.route('/api/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_planet_to_favorite(planet_id):
+    favorite = Favorite()
+    favorite.favorite_type = "planet"
+    favorite.favorite_id = planet_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify("Successfully added"), 201
+
+@app.route('/api/favorite/people/<int:people_id>', methods=['POST'])
+def add_people_to_favorite(people_id):
+    favorite = Favorite()
+    favorite.favorite_type = "people"
+    favorite.favorite_id = people_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify("Successfully added"), 201
+
+@app.route('/api/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_at_favorites(planet_id):
+    planet = Planet.query.get(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return jsonify({ "success": "Planet deleted from favorites"}), 200
+
+@app.route('/api/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_planet_at_favorites(planet_id):
+    people = People.query.get(planet_id)
+    db.session.delete(people)
+    db.session.commit()
+
+    return jsonify({ "success": "People deleted from favorites"}), 200
+
+if __name__ == '__main__':
+    app.run()
--- src/models.py
+++ src/models.py
@@ -48,3 +48,30 @@ class Planet(db.Model):
             "terrain": self.terrain,
             "surface_water": self.surface_water
         }
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_type = db.Column(db.String(10), nullable=False)
+    favorite_id = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "favorite_type": self.favorite_type,
+            "favorite_id": self.favorite_id
+        }
